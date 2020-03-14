391123daa8d25904f03996b6a05fde48b5d4d324
richards12306 <richards12306@gmail.com>
2020-03-14 16:06:50 +0800

    try in vscode

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 #下级路由文件 上级处理后的 url 文件会匹配进入下级进行处理。
 from django.urls import path
-from . import views 
+from . import views
 urlpatterns = [
-    path('',views.blog_list,name = "blog_list"),
-    path('<int:blog_id>',views.blog_details,name="blog_detail"),
-    path('type/<int:blog_type_pk>',views.blog_with_type,name = "blog_with_type"),
-    path('date/<int:year>/<int:month>',views.blog_with_date, name="blog_with_date"),
+    path('', views.blog_list, name="blog_list"),
+    path('<int:blog_id>', views.blog_details, name="blog_detail"),
+    path('type/<int:blog_type_pk>',
+         views.blog_with_type,
+         name="blog_with_type"),
+    path('date/<int:year>/<int:month>',
+         views.blog_with_date,
+         name="blog_with_date"),
+    # path('asdfasfdafads')
 ]
