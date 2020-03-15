0b5c6d2527a079a0c941b53387013a1c4e99e2ed
richards12306 <richards12306@gmail.com>
2020-03-15 11:15:24 +0800

    添加近 7 日阅读数据和主页图表

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from read_statics.models import ReadNum, ReadDetail
+from read_statics.utils import get_week_read_data
+from blog.models import Blog
 
-def home(request): 
+
+def home(request):
     context = {}
+    blog_content_type = ContentType.objects.get_for_model(Blog)
+    dates, read_nums = get_week_read_data(blog_content_type)
 
-    return render(request,'home.html',context)
+
+    context = {}
+    context['read_nums'] = read_nums
+    context['dates'] = dates
+    return render(request, 'home.html', context)
--- read_statics/utils.py
+++ read_statics/utils.py
@@ -1,5 +1,8 @@
 from django.contrib.contenttypes.models import ContentType
-from .models import ReadNum
+from .models import ReadNum, ReadDetail
+from django.utils import timezone
+import datetime
+from django.db.models import Sum
 
 
 def read_statistics_once_read(request, obj):
@@ -8,12 +11,32 @@ def read_statistics_once_read(request, obj):
 
     if not request.COOKIES.get(key):
         #判断是否存在对应的阅读数量
+        #总阅读数
+        readnum, created = ReadNum.objects.get_or_create(content_type=ct,
+                                                         object_id=obj.pk)
 
-        if ReadNum.objects.filter(content_type=ct, object_id=obj.pk).count():
-            readnum = ReadNum.objects.get(content_type=ct, object_id=obj.pk)
-
-        else:
-            readnum = ReadNum(content_type=ct, object_id=obj.pk)
         readnum.read_num += 1
         readnum.save()
+        #当天阅读数
+        date = timezone.now().date()
+        readDetail, created = ReadDetail.objects.get_or_create(
+            content_type=ct, object_id=obj.pk, date=date)
+        readDetail.read_num += 1
+        readDetail.save()
+
     return key
+
+
+def get_week_read_data(content_type):
+    today = timezone.now().date()
+    read_nums = []
+    dates = []
+    for i in range(6, -1, -1):
+        date = today - datetime.timedelta(days=i)
+        dates.append(date.strftime("%m/%d"))
+
+        read_details = ReadDetail.objects.filter(content_type=content_type,
+                                                 date=date)
+        result = read_details.aggregate(read_num_sum=Sum('read_num'))
+        read_nums.append(result['read_num_sum'] or 0)
+    return dates, read_nums
